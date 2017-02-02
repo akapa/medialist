58e9aec0bc328bb7b72ae6cf5a6e41088c3e711a
Richard Nagy <tubi.kapa@gmail.com>
2017-02-02 11:24:41 -0800

    ApiMediaSource provides convenience layer for sort/filter.

--- src/ApiMediaSource.js
+++ src/ApiMediaSource.js
@@ -12,6 +12,31 @@ export default class ApiMediaSource extends DataSource {
 			url: this.url,
 			dataType: 'jsonp'
 		};
-		return new Promise((resolve, reject) =>	$.ajax(options).done(resolve).fail(reject));
+		return new Promise((resolve, reject) =>	{
+			$.ajax(options)
+				.done((results) => {
+					this.data = results;
+					resolve(results);
+				})
+				.fail(reject);
+		});
+	}
+
+	applyFilters(sortProp, sortDirection, filters) {
+		const getPropertyValue = (obj, name) => {
+			const value = obj[name];
+			if (value instanceof Date) return value.getTime();
+			if (typeof value === 'object') return JSON.stringify(value);
+			return value;
+		};
+
+		const dir = sortDirection === 'asc' ? 1 : -1;
+		this.sort = (elem1, elem2) => {
+			const p1 = getPropertyValue(elem1, sortProp);
+			const p2 = getPropertyValue(elem2, sortProp);
+			if (p1 > p2) return dir;
+			if (p1 < p2) return -dir;
+			return 0;
+		};
 	}
 }
