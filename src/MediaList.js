791e28e24aed02f4da5ec005b0d8228eb2ccae2f
Richard Nagy <tubi.kapa@gmail.com>
2017-02-02 14:50:14 -0800

    Tweaks on media list.

--- src/MediaList.js
+++ src/MediaList.js
@@ -10,21 +10,23 @@ export default class MediaList extends View {
 	template() {
 		return `
 			<ul class="list">
-			${this.data.media.map((elem) => !elem ? '' : `
+			${this.data.media.map((elem) => !elem || !elem.id ? '' : `
 				<li data-id="${elem.id}" class="media ${elem.type} ${elem.isLive ? 'is-live' : ''}">
 					<img class="picture" src="${elem.picture}" alt="" />
 					<h2 class="title">${elem.title}</h2>
 					<p class="description">${elem.description}</p>
 					<ul class="labels">
-					${elem.labels && elem.labels.map(label => `
+					${elem.labels ? elem.labels.map(label => `
 						<li class="label" tabindex="0">${label}</li>
-					`).join('')}
+					`).join('') : ''}
 					</ul>
+					${elem.location ? `
 					<p class="location">
 						<span class="city">${elem.location.city}</span>
 						<span class="country">${elem.location.country}</span>
 					</p>
-					<p class="viewers">${elem.viewers}</p>
+					` : ''}
+					<p class="viewers"><i class="icon icon-users"></i> ${elem.viewers || 0}</p>
 				</li>
 			`).join('')}
 			</ul>
