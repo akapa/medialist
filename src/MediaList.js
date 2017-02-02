40e2665cacdaa5415d706e9f82f70d6458114b0e
Richard Nagy <tubi.kapa@gmail.com>
2017-02-02 11:23:06 -0800

    Handling errors stemming from bad data.

--- src/MediaList.js
+++ src/MediaList.js
@@ -10,13 +10,13 @@ export default class MediaList extends View {
 	template() {
 		return `
 			<ul class="list">
-			${this.data.media.map((elem) => `
+			${this.data.media.map((elem) => !elem ? '' : `
 				<li data-id="${elem.id}" class="media ${elem.type} ${elem.isLive ? 'is-live' : ''}">
 					<img class="picture" src="${elem.picture}" alt="" />
 					<h2 class="title">${elem.title}</h2>
 					<p class="description">${elem.description}</p>
 					<ul class="labels">
-					${elem.labels.map(label => `
+					${elem.labels && elem.labels.map(label => `
 						<li class="label" tabindex="0">${label}</li>
 					`).join('')}
 					</ul>
@@ -24,6 +24,7 @@ export default class MediaList extends View {
 						<span class="city">${elem.location.city}</span>
 						<span class="country">${elem.location.country}</span>
 					</p>
+					<p class="viewers">${elem.viewers}</p>
 				</li>
 			`).join('')}
 			</ul>
